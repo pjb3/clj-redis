85703cfa67c13738fc89efa5c8199ca45bfebdff
Paul Barry <mail@paulbarry.com>
2012-03-23 11:47:13 -0400

    Updated project info so I can push to clojars

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-redis "0.0.13-SNAPSHOT"
+(defproject org.clojars.pjb3/clj-redis "0.0.14"
   :description "Clojure Redis client library"
-  :url "https://github.com/mmcgrana/clj-redis"
+  :url "https://github.com/pjb3/clj-redis"
   :dependencies [[org.clojure/clojure "[1.2.0,1.3.0]"]
                  [redis.clients/jedis "1.5.2"]]
   :multi-deps {"1.3" [[org.clojure/clojure "1.3.0"]
